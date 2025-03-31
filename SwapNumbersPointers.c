34c74c93d912945bc2ba543214a3587f961062fa
Aline Nunes Watanabe <linenwatanabe@gmail.com>
2025-03-31 17:08:19 -0300

    pointers

---
+++ SwapNumbersPointers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(){
+    int a;
+    int b;
+
+    printf("Type the first number: ");
+    scanf("%d", &a);
+
+    printf("Type the second number: ");
+    scanf("%d", &b);
+
+    swap(&a, &b);
+
+    printf("Swap was done.\n");
+    printf("The first number now is %d\n", a);
+    printf("The second number now is %d", b);
+}
+
+void swap(int *x, int *y){
+    int aux = *x;
+
+    *x = *y;
+    *y = aux;     
+}
